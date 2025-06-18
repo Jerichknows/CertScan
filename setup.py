050996a9716652a505a381bc466fef522aa9cdf0
Jerichknows <jerichradam@gmail.com>
2025-06-18 19:42:21 +0800

    third commit

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ setup(
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        alembic==1.13.1
-        blinker==1.7.0
-        click==8.1.7
-        Flask==2.3.2
-        Flask-Login==0.6.2
-        Flask-Migrate==4.0.5
-        Flask-SQLAlchemy==3.0.5
-        python-dotenv==1.0.0
-        SQLAlchemy==2.0.23  # Working version with Python 3.11
-        Werkzeug==2.3.7
-        qrcode==7.4.2
-        Pillow==10.0.1 --only-binary=:all:
-        qreader==3.16
-        numpy==1.26.4
-        opencv-python-headless==4.8.1.78
-        pyzbar==0.1.9
-        gunicorn==21.2.0
+alembic==1.13.1
+blinker==1.7.0
+click==8.1.7
+Flask==2.3.2
+Flask-Login==0.6.2
+Flask-Migrate==4.0.5
+Flask-SQLAlchemy==3.0.5
+python-dotenv==1.0.0
+SQLAlchemy==2.0.23
+Werkzeug==2.3.7
+qrcode==7.4.2
+Pillow==10.2.0 --only-binary=:all:  # Available version with binary wheels
+qreader==3.16
+numpy==1.26.4
+opencv-python-headless==4.8.1.78
+pyzbar==0.1.9
+gunicorn==21.2.0
     ],
 )
