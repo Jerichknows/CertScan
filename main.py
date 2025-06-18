debe14e713370c9ec5a7e71876f9588e781e0e35
Jerichknows <jerichradam@gmail.com>
2025-06-18 11:42:04 +0800

    second commit

--- main.py
+++ main.py
@@ -222,4 +222,5 @@ def unverified_certificates():
                          title="Unverified Certificates")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
